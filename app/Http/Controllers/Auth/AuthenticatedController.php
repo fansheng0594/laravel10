d198f864736b4be7021d6a5f57e23a31c933df70
Append <32379148+fansheng0594@users.noreply.github.com>
2023-02-20 10:32:55 +0800

    signin remember_me

--- app/Http/Controllers/Auth/AuthenticatedController.php
+++ app/Http/Controllers/Auth/AuthenticatedController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
@@ -23,8 +24,8 @@ class AuthenticatedController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]); 
-
-        if (! Auth::attempt($request->only(['email', 'password']))) {
+        
+        if (! Auth::attempt($request->only(['email', 'password']), $request->boolean('remember'))) {
             throw ValidationException::withMessages([
                 'email' => trans('validation.auth.failed'),
             ]);
